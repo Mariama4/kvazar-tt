4115a4d75129ae273b652ccfab533436dd129e9f
mariama4 <mariama4@mail.ru>
2023-04-26 23:23:52 +0700

    create user model

---
+++ user/userModel.py
@@ -0,0 +1,15 @@
+from db import db
+from datetime import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class UserModel(db.Model):
+    """User Model"""
+
+    __tablename__ = "User"
+
+    id: db.Column[db.Integer] = db.Column(db.Integer, primary_key=True)
+    username: db.Column[db.String] = db.Column(db.String(45), nullable=False)
+    email: db.Column[db.String] = db.Column(db.String(45), unique=True, nullable=False)
+    registration_date: db.Column[db.DateTime] = db.Column(db.DateTime, default=datetime.now)
