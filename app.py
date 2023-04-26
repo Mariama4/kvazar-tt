1badfdee567670b9cc2cb05f2a13ff3604b21834
mariama4 <mariama4@mail.ru>
2023-04-26 23:24:35 +0700

    add init all SQLAlchemy models in app

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig if app.config["DEBUG"] else ProductionConfig)
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run()
