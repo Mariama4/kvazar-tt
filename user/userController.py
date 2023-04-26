53f76eb4d9834e9348cc30ac61d8717867233a6a
mariama4 <mariama4@mail.ru>
2023-04-27 00:48:26 +0700

    implement user controller method - getAll

--- user/userController.py
+++ user/userController.py
@@ -1,4 +1,4 @@
-from flask import Response
+from flask import request, Response, jsonify
 
 from interfaces import IUserController
 from interfaces import IUserService
@@ -9,19 +9,33 @@ class UserController(IUserController):
     """Represents a controller for the user resource in application.
 
     Attributes:
-        userService: Service class to handle the user resource.
+        userService: Service class used to handle the user resource.
     """
     userService: IUserService = UserService()
-    def getAll(self) -> Response: ...
 
-    def create(self) -> Response: ...
+    def getAll(self) -> Response:
+        """Gets all users or paginated users from database.
 
-    def getById(self, id: int) -> Response: ...
+        :return: Flask `Response` object containing the JSON representation of the users.
+        """
+        page = request.args.get("page", None, type=int)
+        per_page = request.args.get("per_page", None, type=int)
 
-    def updateById(self, id: int) -> Response: ...
+        if page is None or per_page is None:
+            users = self.userService.getAll()
+        else:
+            users = self.userService.getPaginatedUsers(page, per_page)
 
-    def delete(self, id: int) -> Response: ...
+        return jsonify(users)
 
+    def create(self) -> Response:
+        ...
 
+    def getById(self, id: int) -> Response:
+        ...
 
+    def updateById(self, id: int) -> Response:
+        ...
 
+    def delete(self, id: int) -> Response:
+        ...
