c6f6028e471e0e09973cf6385677e1647a714b3a
mariama4 <mariama4@mail.ru>
2023-04-27 01:06:04 +0700

    implement user endpoint `GET /users/<int>`

--- user/userRouter.py
+++ user/userRouter.py
@@ -32,3 +32,9 @@ class UserRouter(IUserRouter):
             view_func=self.userController.create,
             methods=["POST"]
         )
+
+        self.app.add_url_rule(
+            rule=f"/{name}/<int:id>",
+            view_func=self.userController.getById,
+            methods=["GET"],
+        )
