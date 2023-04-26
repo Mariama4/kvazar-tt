2cb2661450430748dec0a88dc7705f99749f1fcd
mariama4 <mariama4@mail.ru>
2023-04-26 23:12:41 +0700

    add sqlalchemy to project

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from config import DevelopmentConfig, ProductionConfig
+from db import db
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig if app.config["DEBUG"] else ProductionConfig)
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run()
