8d41f28343d12db35d24aee1e1ef485d9fcea2b6
mariama4 <mariama4@mail.ru>
2023-04-27 17:38:55 +0700

    сreating a function that returns the top 5 users with the longest usernames

---
+++ user/utils/getTop5UsersWithLongestNames.py
@@ -0,0 +1,12 @@
+from typing import Type
+from ..userModel import UserModel
+
+
+def get_top_5_users_with_longest_names(user_model: Type[UserModel]) -> list[UserModel]:
+    """ Getting the top 5 users with the longest usernames (sorted in descending order).
+
+    :param user_model: User model representing the user object.
+    :return: List of `UserModel` objects representing the users.
+    """
+    users = user_model.query.order_by(user_model.username.desc()).limit(5).all()
+    return users
