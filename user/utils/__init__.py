706a4caefe5dfe06007229b81f757caa56c13965
mariama4 <mariama4@mail.ru>
2023-04-27 23:35:02 +0700

    add user utils

---
+++ user/utils/__init__.py
@@ -0,0 +1,9 @@
+from .getCountUsersRegisteredLastWeek import get_count_users_registered_last_week
+from .getTop5UsersWithLongestNames import get_top_5_users_with_longest_names
+from .getDomainEmailRatio import get_domain_email_ratio
+
+__all__ = [
+    "get_count_users_registered_last_week",
+    "get_top_5_users_with_longest_names",
+    "get_domain_email_ratio",
+]
