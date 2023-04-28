09a2c2f560fa99716470ea60c1a83a744188eb87
mariama4 <mariama4@mail.ru>
2023-04-28 14:10:37 +0700

    edit folder name from user to users

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from db import db
 from config import DevelopmentConfig, ProductionConfig
-from user import *
+from users import *
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig if app.config["DEBUG"] else ProductionConfig)
