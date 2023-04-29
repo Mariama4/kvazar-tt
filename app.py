0a149f54889634dff45ceb645f1561ce789a0c76
mariama4 <mariama4@mail.ru>
2023-04-29 17:11:29 +0700

    use application factories in app

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
 from flask import Flask
 from flask_swagger_ui import get_swaggerui_blueprint
 from db import db
-from config import DevelopmentConfig, ProductionConfig
 from users import *
 
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig if app.config["DEBUG"] else ProductionConfig)
 
-SWAGGER_URL = "/api/docs"  # URL for exposing Swagger UI (without trailing '/')
-API_URL = "/static/swagger.json"  # Our API url (can of course be a local resource)
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
 
-# Call factory function to create our blueprint
-swagger_ui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
-    API_URL,
-    config={"app_name": __name__},  # Swagger UI config overrides
-)
+    SWAGGER_URL = "/api/docs"  # URL for exposing Swagger UI (without trailing '/')
+    API_URL = "/static/swagger.json"  # Our API url (can of course be a local resource)
 
-db.init_app(app)
+    # Call factory function to create our blueprint
+    swagger_ui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+        API_URL,
+        config={"app_name": __name__},  # Swagger UI config overrides
+    )
 
-with app.app_context():
-    db.create_all()
+    db.init_app(app)
 
-app.register_blueprint(swagger_ui_blueprint)
-app.register_blueprint(users_app)
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(swagger_ui_blueprint)
+    app.register_blueprint(users_app)
+
+    return app
+
 
 if __name__ == "__main__":
-    app.run()
+    from config import DevelopmentConfig
+
+    user_app = create_app(DevelopmentConfig)
+    user_app.run()
